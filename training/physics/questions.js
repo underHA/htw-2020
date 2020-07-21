bb52d10e23755a69f228e4e57051defe874a3d33
toblrne <67180069+toblrne@users.noreply.github.com>
2020-07-21 02:50:49 -0700

    Formatted questions in JS

---
+++ training/physics/questions.js
@@ -0,0 +1,54 @@
+questions = [
+  {"topic":"Uniform Motion", "questions":[
+    "A car must be driven a distance of 120km in 2.5h. During the first 1.5h the average speed was 70 kmh-1. Calculate the average speed for the remainder of the journey.",
+    "The initial velocity of a car moving on a straight road is 2.0ms−1. It becomes 8.0ms−1 after travelling for 2.0s under constant acceleration. Find the acceleration.",
+    "The initial velocity of a car moving on a straight road is 2.0ms−1. It becomes 8.0ms−1 after travelling for 2.0s under constant acceleration. Find the acceleration.",
+    "A particle has an initial velocity of 12ms−1 and is brought to rest over a distance of 45m. Find the acceleration of the particle.",
+    "A particle at the origin has an initial velocity of −6.0ms−1 and moves with an acceleration of 2.0ms−2. Determine when its position will become 16m.",
+    "A plane starting from rest takes 15.0s to take off after speeding over a distance of 450m on the runway with constant acceleration. Find the take-off velocity.",
+    "Two balls are dropped from rest from the same height. One of the balls is dropped 1.00s after the other. Find the distance that separates the two balls 2.00s after the second ball is dropped.",
+    "The initial velocity of a car moving on a straight road is 2.0ms−1. It becomes 8.0ms−1 after travelling for 2.0s under constant acceleration. Find the acceleration.",
+    "A particle has an initial velocity of 12ms−1 and is brought to rest over a distance of 45m. Find the acceleration of the particle.",
+    "A particle at the origin has an initial velocity of −6.0ms−1 and moves with an acceleration of 2.0ms−2. Determine when its position will become 16m.",
+    "A plane starting from rest takes 15.0s to take off after speeding over a distance of 450m on the runway with constant acceleration. Find the take-off velocity.",
+    "Two balls are dropped from rest from the same height. One of the balls is dropped 1.00s after the other. Find the distance that separates the two balls 2.00s after the second ball is dropped.",
+    "A stone is thrown vertically up from the edge of a cliff 35.0m from the sea. The initial velocity of the stone is 8.00ms−1. Determine the maximum height of the stone, the time when it hits the sea, the velocity just before hitting the sea, the distance the stone covers, and the average speed and the average velocity for this motion."
+  ]},
+
+  {"topic":"Projectile Motion", "questions":[
+    "A ball rolls off a table with a horizontal speed of 2.0ms−1.The table is 1.3m high. Calculate how far from the table the ball will land.",
+    "Two particles are on the same vertical line.They are thrown horizontally with the same speed, 4.0ms−1, from heights of 4.0m and 8.0m. Calculate the distance that will separate the two objects when both land on the ground. The particle at the 4.0m height is now launched with horizontal speed u such that it lands at the same place as the particle launched from 8.0m. Calculate u.",
+    "Determine the maximum height reached by an object thrown with speed 24ms−1 at 40° to the horizontal.",
+    "A stone is thrown with a speed of 20.0ms−1 at an angle of 48° to the horizontal from the edge of a cliff 60.0m above the surface of the sea. Calculate the velocity with which the stone hits the sea."
+  ]},
+
+  {"topic":"Equilibrium", "questions":[
+    "A mass of 2.00kg rests on a rough horizontal table.The coefficient of static friction between the block and the table is 0.60.The block is attached to a hanging mass by a string that goes over a smooth pulley, as shown in the diagram. Determine the largest mass that can hang in this way without forcing the block to slide.",
+    "A girl tries to lift a suitcase of weight 220N by pulling upwards on it with a force of 140N.The suitcase does not move. Calculate the reaction force from the floor on the suitcase.",
+    "A block of mass 15.0kg rests on a horizontal table. A force of 50.0N is applied vertically downward on the block. Calculate the force that the block exerts on the table."
+  ]},
+
+  {"topic":"Accelerated Motion", "questions":[
+    "A car of mass 1400 kg is on a muddy road. If the force from the engine pushing the car forward exceeds 600N, the wheels slip (i.e. they rotate without rolling). Estimate the car’s maximum acceleration on this road.",
+    "A mass of 3.0kg is acted upon by three forces of 4.0N, 6.0N and 9.0N and is in equilibrium. Convince yourself that these forces can indeed be in equilibrium.The 9.0N force is suddenly removed. Determine the acceleration of the mass."
+  ]},
+
+  {"topic":"Work, Energy, and Power", "questions":[
+    "A horizontal force of 24N pulls a body a distance of 5.0m along its direction. Calculate the work done by the force.",
+    "A block slides along a rough table and is brought to rest after travelling a distance of 2.4m. A force of 3.2N opposes the motion. Calculate the work done by the opposing force.",
+    "A block of mass 2.0kg and an initial speed of 5.4ms−1 slides on a rough horizontal surface and is eventually brought to rest after travelling a distance of 4.0m. Calculate the frictional force between the block and the surface.",
+    "A spring of spring constant k=200Nm−1 is slowly extended from an extension of 3.0cm to an extension of 5.0cm. Calculate the work done by the extending force.",
+    "The engine of a car is developing a power of 90kW when it is moving on a horizontal road at a constant speed of 100 km h−1. Estimate the total horizontal force opposing the motion of the car.",
+    "The motor of an elevator develops power at a rate of 2500 W. Calculate the speed that a 1200 kg load is being raised at.",
+    "The top speed of a car whose engine is delivering 250kW of power is 240kmh−1. Calculate the value of the resistance force on the car when it is travelling at its top speed on a level road."
+  ]},
+
+  {"topic":"Momentum", "questions":[
+    "The momentum of a ball increased by 12.0Ns as a result of a force that acted on the ball for 2.00s. Find the average force on the ball.",
+    "Two masses of 2.0kg and 4.0kg are held in place, compressing a spring between them.When they are released, the 2.0 kg moves away with a speed of 3.0ms−1. What was the energy stored in the spring?",
+    "A rocket in space where gravity is negligible has a mass (including fuel) of 5000 kg. It is desired to give the rocket an average acceleration of 15.0ms−2 during the first second of firing the engine. The gases leave the rocket at a speed of 1500 m s−1 (relative to the rocket). Estimate how much fuel must be burnt in that second."
+  ]}
+
+
+
+]
